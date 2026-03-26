7e0cc8bbb45f47cb1e3578c0ba7705e08f318a2d
fernandorosero91 <elier.rosero@campusucc.edu.co>
2026-03-26 13:37:20 -0500

    manejo de errores de rutas y templates

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'appDev4'
+    'appDev4',
     'appDev1',
     'appDev3',
     'appDev2',
